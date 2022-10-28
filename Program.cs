27ae8e085659ecc1ee0303810d593075ac1f3227
F_D <den9152@yandex.ru>
2022-10-28 20:21:11 +0700

    2
    2

--- Program.cs
+++ Program.cs
@@ -9,3 +9,22 @@
 // ==  роверка на равенство площадей магазинов
 // < > проверка на больше или меньше площади магазина
 // != и Equals
+
+class shop
+{
+    string _name;
+    string _description;
+    int _sqare;
+
+    public shop (string name, string descriptions, int sqare)
+    {
+        _name = name;
+        _description = descriptions;
+        _sqare = sqare;
+    }
+    public int Get_sqare(int sqare)
+    {
+        int _sqare = sqare;
+        return _sqare;
+    }
+}
