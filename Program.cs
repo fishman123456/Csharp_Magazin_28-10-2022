ecb595011c7acd1ff9480ebdf709ccaad73e7c0b
fishman123456 <den9152@yandex.ru>
2022-10-30 21:35:29 +0700

    дома доделал
    добавил сравнение площадей, вывод на консоль

--- Program.cs
+++ Program.cs
@@ -10,38 +10,82 @@
 // < > проверка на больше или меньше площади магазина
 // != и Equals
 
-shop f = new shop("dfgsd","dfs",8);
+using System.Diagnostics;
+
+shop f = new shop("Рыбный","Бардина-4",8);
 
 Console.WriteLine(f + 4);
+shop one = new shop("первый", "рыбный", 2);
+Console.WriteLine(one);
+shop two = new shop("Молочный", "Комсомольская площадка", 2);
+Console.WriteLine(two);
+if (one == one)
+{
+    Console.WriteLine("==");
+}
+else Console.WriteLine("=!");
+f.print(two);
 
 class shop
 {
     string _name;
     string _description;
-    int _sqare;
+    int _square;
 
-    public shop (string name, string descriptions, int sqare)
+    public shop (string name, string descriptions, int square)
     {
-        _name = name;
-        _description = descriptions;
-        _sqare = sqare;
+        _name = name;// имя магазина
+        _description = descriptions;// пояснение, адрес
+        _square = square;// площадь метров квадратных
     }
-
-
+    public override string ToString()
+    {
+        return $"Магазин \"{this._name}\" по адресу {this._description}, площадью {this._square} м^2.";
+    }
+    public string? Name
+    {
+        get { return this._name; }// принимает значение
+        set { this._name = value; } // берет значение
+    }
+    public string? Description
+    {
+        get { return this._description; }// - // - // - //
+        set { this._description = value; }// - // - // - //
+    }
+    public int? Square
+    { 
+        get { return this._square; } // - // - // - // - //
+        set { this._square = (int)(value == 0 ? 1 : value); } // - // - // - //
+}
     public static shop operator +(shop v1,int d)
     {
-        v1._sqare += d;
+        v1._square += d;
         return v1;
        
     }
 
-    public static bool operator ==(shop? left, shop? right)
+    public static bool operator ==(shop left, shop right)
     {
-        return EqualityComparer<shop>.Default.Equals(left, right);
+        if (left._square == right._square)
+        {
+            Console.WriteLine("Площади равны");
+                return true;
+        }
+        Console.WriteLine("Площади не совпадают");
+        return false;
     }
 
     public static bool operator !=(shop? left, shop? right)
     {
         return !(left == right);
+    }
+    public void print(shop shop)
+    {
+        Console.WriteLine(shop);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
